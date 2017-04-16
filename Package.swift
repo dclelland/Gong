5110be94eceabb55f3da53bf4438d778901e3ca0
Daniel Clelland <daniel.clelland@gmail.com>
2017-04-17 00:18:20 +1200

    Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  Gong
+//
+//  Created by Daniel Clelland on 17/04/17.
+//  Copyright © 2017 Daniel Clelland. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Gong"
+)
