685d88afb2eee2966b6c0609a262ec81434b5f5e
Daniel Clelland <daniel.clelland@gmail.com>
2017-04-25 16:54:29 +1200

    Stash

--- Sources/Units/MIDIScale.swift
+++ Sources/Units/MIDIScale.swift
@@ -27,4 +27,6 @@ extension MIDIScale {
     public static let major: MIDIScale = [.P1, .M2, .M3, .P4, .P5, .M6, .M7]
     public static let minor: MIDIScale = [.P1, .M2, .m3, .P4, .P5, .m6, .m7]
     
+    public static let chromatic: MIDIScale = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    
 }
