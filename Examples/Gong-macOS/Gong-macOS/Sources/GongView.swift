b5f6429aaa9c8232d5c7bfca1b2e0b0dcfe593b5
Daniel Clelland <daniel.clelland@gmail.com>
2022-05-17 14:03:55 +1200

    Install Gong remotely; update SwiftUI view

--- Examples/Gong-macOS/Gong-macOS/Sources/GongView.swift
+++ Examples/Gong-macOS/Gong-macOS/Sources/GongView.swift
@@ -6,12 +6,37 @@
 //
 
 import SwiftUI
+import Gong
 
 struct GongView: View {
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack(spacing: 8.0) {
+            Button(action: { playNote(c5) }, label: { Text("C").frame(width: 24.0) })
+            Button(action: { playNote(d5) }, label: { Text("D").frame(width: 24.0) })
+            Button(action: { playNote(e5) }, label: { Text("E").frame(width: 24.0) })
+            Button(action: { playNote(f5) }, label: { Text("F").frame(width: 24.0) })
+            Button(action: { playNote(g5) }, label: { Text("G").frame(width: 24.0) })
+            Button(action: { playNote(a5) }, label: { Text("A").frame(width: 24.0) })
+            Button(action: { playNote(b5) }, label: { Text("B").frame(width: 24.0) })
+        }
+        .padding()
+    }
+    
+}
+
+extension GongView {
+    
+    func playNote(_ pitch: Int) {
+//        guard let output = MIDI.output else {
+//            return
+//        }
+//
+//        let note = MIDINote(pitch: pitch)
+//
+//        for device in MIDIDevice.all {
+//            device.send(note, via: output)
+//        }
     }
     
 }
