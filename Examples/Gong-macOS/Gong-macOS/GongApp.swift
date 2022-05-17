aade798dd78ea18f26445733de4d33422ad01de8
Daniel Clelland <daniel.clelland@gmail.com>
2022-05-17 13:45:40 +1200

    Create new SwiftUI project

---
+++ Examples/Gong-macOS/Gong-macOS/GongApp.swift
@@ -0,0 +1,18 @@
+//
+//  Gong_macOSApp.swift
+//  Gong-macOS
+//
+//  Created by Daniel Clelland on 17/05/22.
+//
+
+import SwiftUI
+
+@main struct GongApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            GongView()
+        }
+    }
+    
+}
---
+++ Examples/Gong-macOS/Gong-macOS/GongView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Gong-macOS
+//
+//  Created by Daniel Clelland on 17/05/22.
+//
+
+import SwiftUI
+
+struct GongView: View {
+    
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+    
+}
+
+struct GongView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        GongView()
+    }
+    
+}
