1d10067674cddc5a3402d1de7977d9090be6298a
Daniel Clelland <daniel.clelland@gmail.com>
2022-05-17 14:49:59 +1200

    Remove old project

--- Examples/Gong-macOS-old/Pods/Target Support Files/Gong/Gong-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Gong : NSObject
-@end
-@implementation PodsDummy_Gong
-@end
--- Examples/Gong-macOS-old/Pods/Target Support Files/Pods-Gong-macOS/Pods-Gong-macOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Gong_macOS : NSObject
-@end
-@implementation PodsDummy_Pods_Gong_macOS
-@end
